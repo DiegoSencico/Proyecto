d97231d013915b6f923963c77966456c5f1d62a4
DiegoSencico <48455278@sencico.edu.pe>
2023-12-11 10:03:48 -0500

    se creo una funcion con la descripcion del proyecto en js

--- codigo.js
+++ codigo.js
@@ -6,6 +6,9 @@ function Proyecto (){
     console.log ("Proyecto de la Municipalidad");
 }
 
+function DescripcionProy(){
+    console.log ("Este proyecto es para la municipalidad hecho en gitbash para despues subirlo al gitgub");
+}
 
 
 
